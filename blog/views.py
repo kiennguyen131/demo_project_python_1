c99ec4ed1d6c7a0b498da8b1f1e49e10063f8d3f
kiennt-1127 <nguyen.trung.kien-b@sun-asterisk.com>
2022-10-30 23:42:51 +0700

    using template

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
-def list(request):
-  Data = {'Post': Post.objects.all().order_by('-date')}
-  return render(request, 'blog/blog.html', Data)
+# def list(request):
+#   Data = {'Post': Post.objects.all().order_by('-date')}
+#   return render(request, 'blog/blog.html', Data)
 
-def post(request, id):
-  post = Post.objects.get(id=id)
-  return render(request, 'blog/post.html', {'post': post})
+class PostListView(ListView):
+  queryset = Post.objects.all().order_by('-date')
+  template_name = 'blog/blog.html'
+  context_object_name = 'Post'
+  paginate_by = 1
+
+# def post(request, id):
+#   post = Post.objects.get(id=id)
+#   return render(request, 'blog/post.html', {'post': post})
+
+class PostDetailView(DetailView):
+  model = Post
+  template_name = 'blog/post.html'

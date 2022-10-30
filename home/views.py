65bcb81053e5aff6ee40e05a484df5220f9c23ba
kiennt-1127 <nguyen.trung.kien-b@sun-asterisk.com>
2022-10-30 22:00:35 +0700

    register form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,20 @@
 from curses.ascii import HT
 from urllib import response
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import RegistrationForm
 
 def index(request):
   return render(request, 'pages/home.html')
 
 def contact(request):
   return render(request, 'pages/contact.html')
+
+def register(request):
+  form = RegistrationForm()
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect('/')
+  return render(request, 'pages/register.html', {'form': form})
